4126f4515269e2d2ea4c7cc756535881c330111a
Corwin <gmail@corwincheung.com>
2024-11-14 11:38:05 -0500

    run script

--- transformer/run.sh
+++ transformer/run.sh
@@ -6,17 +6,17 @@
 # SBATCH --ntasks-per-node=1
 # SBATCH --gpus-per-node=1
 # SBATCH --cpus-per-task=8
-# SBATCH --time=1:00:00  # Adjust based on your needs
+# SBATCH --time=00:10:00  # Adjust based on your needs
 # SBATCH --mem=12GB
 # SBATCH --partition=kempner
-# SBATCH --array=1-1%2  # Adjust based on the number of hyperparameter combinations
+# SBATCH --array=1-2%4
 
 module load cuda/11.7
 module load anaconda
 source activate transformers  # Replace with your conda environment
 
 # Read hyperparameters from CSV
-IFS=',' read -r sequence_length n_layer n_head n_embd epochs learning_rate <<< $(sed -n "$((SLURM_ARRAY_TASK_ID + 1))"p cleaned_hyperparameters.csv)
+IFS=',' read -r sequence_length n_layer n_head n_embd epochs learning_rate <<< $(sed -n "$((SLURM_ARRAY_TASK_ID + 1))"p cleaned_hyperparameters.csv
 
 # Run your training script with hyperparameters
 python train.py \
