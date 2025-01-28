9e3fa12ca1b1baca2a9cab78a6f51835a9ded22b
Celia <celiaberon@gmail.com>
2025-01-28 12:35:31 -0500

    refactoring pp for learning predictions

--- transformer/inference/learning.py
+++ transformer/inference/learning.py
@@ -15,7 +15,7 @@ behavior_helpers_path = code_path / 'behavior-helpers'
 sys.path.append(f'{str(behavior_helpers_path)}/')
 from bh.visualization import plot_trials as pts
 
-from evaluation.graph_helper import map_rl_to_pattern
+from evaluation.graph_helper import add_sequence_columns
 from evaluation.graphs_on_trial_block_transitions import parse_files
 
 
@@ -23,21 +23,37 @@ def load_predictions(run=None, model_name=None):
 
     """Load and process all prediction files for a run efficiently."""
     run = run or get_latest_run()
-    # Load predictions    # Get model info from metadata
+
+    # Get model info from metadata.
     model_info = parse_model_info(run, model_name=model_name)
     model_name = model_info['model_name']
 
     pred_file = get_experiment_file(f"learning_{model_name}_val_preds.txt", run)
-    context_file = get_experiment_file(f"learning_{model_name}_val_context.txt", run)
+    # context_file = get_experiment_file(f"learning_{model_name}_val_context.txt", run)
 
     predictions = pd.read_csv(pred_file, sep='\t')
-    # Sort by step and original index
     predictions = predictions.sort_values(['Step', 'Idx'])
 
     return predictions, model_info
 
 
 def plot_bpos_behavior_learning(predictions, model_name, run):
+    """Plot behavioral position analysis of model predictions.
+
+    Calculates and plots switch probability and high port selection probability
+    as a function of position in block, separated by training steps.
+
+    Args:
+        predictions (pd.DataFrame): DataFrame containing model predictions with columns:
+            - Step: Training step number
+            - True/Predicted: True and predicted choices
+            - seqN_RL/seqN: Previous N choices
+        model_name (str): Name of the model for saving plot
+        run (int): Run number for saving plot
+
+    Returns:
+        None. Saves plot to file.
+    """
 
     bpos = pts.calc_bpos_probs(predictions, add_cond_cols=['Step'],
                                add_agg_cols=['pred_Switch', 'pred_selHigh'])
@@ -94,10 +110,7 @@ def main(run=None, model_name=None):
     assert all(predictions['k0'] == predictions['True']), "k0 and True values don't match"
 
     for N in [2, 3]:
-        events[[f'seq{N}_RL', f'seq{N}']] = None
-        events.loc[N:, f'seq{N}_RL'] = [''.join(events['k0'].values[i-N:i])
-                                        for i in range(N, len(events))]
-        events.loc[N:, f'seq{N}'] = events.loc[N:, f'seq{N}_RL'].apply(map_rl_to_pattern)
+        events = add_sequence_columns(events, seq_length=N)
         predictions[f'seq{N}_RL'] = predictions['Idx'].map(events[f'seq{N}_RL'])
         predictions[f'seq{N}'] = predictions['Idx'].map(events[f'seq{N}'])
 
@@ -108,6 +121,7 @@ def main(run=None, model_name=None):
     plot_bpos_behavior_learning(predictions, model_name=model_info['model_name'], run=run)
 
     for N in [2, 3]:
+        # Note, here we can predict on switch because seqN reflects history.
         policies_true = pts.calc_conditional_probs(
             predictions.query('Step==Step.max()'),
             htrials=N, sortby='pevent',
