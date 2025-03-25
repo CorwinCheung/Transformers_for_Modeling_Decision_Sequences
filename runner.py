45456d0f21530ee42e1023447ad3b6a4a177d372
Corwin <gmail@corwincheung.com>
2025-03-25 15:17:45 -0400

    rename run 1 and run 1 mini

--- runner.py
+++ runner.py
@@ -1,13 +1,13 @@
 import os
 
 # Directory containing the files
-directory = "experiments/run_1_mini/seqs"
+directory = "experiments/run_1/seqs"
 
 # Iterate through all files in the directory
 for filename in os.listdir(directory):
-    if "_8" in filename:
+    if "_1_mini" in filename:
         # Create new filename by replacing run_97_ with run_2_
-        new_filename = filename.replace("_8", "_1_mini")
+        new_filename = filename.replace("_1_mini", "_1")
         
         # Create full file paths
         old_file = os.path.join(directory, filename)
